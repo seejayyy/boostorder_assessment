f04621488f44509d5f934f77f88894e9b79f9c98
Chua Jun Jie <cjhuskerz@gmail.com>
2024-12-24 05:23:11 +0800

    Added image in home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -9,30 +9,45 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Boostorder E-commerce App'),
-        backgroundColor: Colors.blue,
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
-      ),
-      body:
-        Center(
-          child: TextButton(
-            onPressed: (){
-              Navigator.pushNamed(context, '/catalogpage');
-            },
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 16)
-            ),
-          child: Text('Catalog', style: TextStyle(fontSize: 18), ),
+        appBar: AppBar(
+          title: Text('Boostorder E-commerce App'),
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Align items tightly in the center
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/boostorder.jpg',
+                height: 256,
+                width: 256,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 24), // Add spacing between the image and button
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/catalogpage');
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(2)),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                ),
+                child: Text(
+                  'Catalog',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+      ));
   }
 }
